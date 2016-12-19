3261150c9822dc31b1d388d6cb7a7c83ef3ce284
AVTrujillo <alexandervtrujillo@gmail.com>
2016-12-18 20:54:17 -0600

    Added gitf model

---
+++ db/migrate/20161219025058_create_gifts.rb
@@ -0,0 +1,18 @@
+class CreateGifts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :gifts do |t|
+      t.integer :id
+      t.string :title
+      t.string :description
+      t.boolean :asked_for
+      t.string :giver
+      t.string :recipient
+      t.string :intend_to_give
+      t.date :shipped
+      t.string :shipping_details
+      t.date :recieved
+
+      t.timestamps
+    end
+  end
+end
