2ca7ec2d5c6cef09c0f70d32de8903cd08b7fe54
AVTrujillo <alexandervtrujillo@gmail.com>
2016-12-19 18:18:50 -0600

    Added all gift views

--- db/migrate/20161219025058_create_gifts.rb
+++ db/migrate/20161219025058_create_gifts.rb
@@ -1,15 +1,15 @@
 class CreateGifts < ActiveRecord::Migration[5.0]
   def change
-    create_table :gifts do |t|
-      t.string :title
-      t.string :description
-      t.boolean :asked_for
-      t.string :giver
-      t.string :recipient
-      t.string :intend_to_give
-      t.date :shipped
-      t.string :shipping_details
-      t.date :recieved
+    create_table  :gifts do |t|
+      t.string    :title
+      t.string    :description
+      t.boolean   :asked_for
+      t.string    :giver
+      t.string    :recipient
+      t.string    :intend_to_give
+      t.date      :shipped
+      t.string    :shipping_details
+      t.date      :recieved
 
       t.timestamps
     end
