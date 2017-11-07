ce6c59b783354ac81c2f94b78b492acfe51e39ac
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-11-07 00:03:48 -0600

    removed all improper uses of "optional: true" in has_many associations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,17 +1,17 @@
 class Friend < ApplicationRecord
 
-  has_many :recipient_matches, foreign_key: :recipient_id, class_name: 'FriendMatch', optional: true
-  has_many :givers, through: :giver_matches, source: :giver, optional: true
+  has_many :recipient_matches, foreign_key: :recipient_id, class_name: 'FriendMatch'
+  has_many :givers, through: :giver_matches, source: :giver
 
-  has_many :giver_matches, foreign_key: :giver_id, class_name: 'FriendMatch', optional: true
-  has_many :recipients, through: :recipient_matches, source: :recipient, optional: true
+  has_many :giver_matches, foreign_key: :giver_id, class_name: 'FriendMatch'
+  has_many :recipients, through: :recipient_matches, source: :recipient
 
-  has_many  :recieved_gifts, foreign_key: :recipient_id, class_name: 'Gift', optional: true
-  has_many  :given_gifts, foreign_key: :giver_id, class_name: 'Gift', optional: true
+  has_many  :recieved_gifts, foreign_key: :recipient_id, class_name: 'Gift'
+  has_many  :given_gifts, foreign_key: :giver_id, class_name: 'Gift'
 
   belongs_to  :generation
 
-  has_many  :forbidden_matches, optional: true
+  has_many  :forbidden_matches
 
   def recipient
     recipients.find {|r| r.year == christmas_year}
--- app/models/generation.rb
+++ app/models/generation.rb
@@ -1,4 +1,4 @@
 class Generation < ApplicationRecord
-	has_many	:friends, optional: true
-	has_many	:friend_matches, through: :friends, source: :giver_matches, optional: true
+	has_many	:friends
+	has_many	:friend_matches, through: :friends, source: :giver_matches
 end
