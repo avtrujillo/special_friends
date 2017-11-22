26c2dbe07a9d3ba88af5169adfda1a60663eee3b
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-11-21 19:24:47 -0600

    minor tweaks to FriendMailer

--- app/mailers/friend_mailer.rb
+++ app/mailers/friend_mailer.rb
@@ -7,20 +7,25 @@ class FriendMailer < ApplicationMailer
 
   def test_email
     mg_client = Mailgun::Client.new Rails.application.secrets.private_mailgun_api
+    mg_events = Mailgun::Events.new(mg_client, mailgun_domain)
     message_params = {
-      from: "messenger_bot@#{Rails.application.secrets.mailgun_domain}",
+      from: "messenger_bot@#{mailgun_domain}",
       to: Rails.application.secrets.my_email,
       subject: "mailer test",
       text: "Test was a success"
     }
-    mg_client.send_message Rails.application.secrets.mailgun_domain, message_params
+    mg_client.send_message mailgun_domain, message_params
   end
 
   private
 
+  def mailgun_domain
+    Rails.application.secrets.mailgun_domain
+  end
+
   def notification_params(fm)
     {
-      from: "special_friends_messenger@#{Rails.application.secrets.mailgun_domain}",
+      from: "special_friends_messenger@#{mailgun_domain}",
       to: fm.recipient.email,
       subject: notification_subject(fm),
       text: notification_text(fm)
