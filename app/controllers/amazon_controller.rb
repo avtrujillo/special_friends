b7cbb72fb4a807204ed3f0fcd16ee896dbb7946e
avtrujillo <alexandervtrujillo@gmail.com>
2018-11-12 21:22:26 -0600

    minor refactoring of AmazonController#create_list

--- app/controllers/amazon_controller.rb
+++ app/controllers/amazon_controller.rb
@@ -6,13 +6,15 @@ class AmazonController < ApplicationController
   def create_list
     list = AmazonWishList.wishlist_from_url(params[:wishlist_url])
     fal = FriendAmazonList.create!(external_id: list.id, friend_id: current_user.id)
+    @duplicates = []
     @wishes = list.wishes.each_with_object(Array.new) do |wish, arr|
       faw = FriendAmazonWish.create!(external_id: wish.id, friend_id: current_user.id,
       friend_amazon_list_id: fal.id, )
       arr << Wish.create!(friend_amazon_wish_id: faw.id,
         friend_id: current_user.id, title: wish.title, description: wish.url)
     end
-    redirect_to "amazon/show_list/#{fal.id}"
+    # redirect_to "amazon/show_list/#{fal.id}"
+    render 'show_import'
   end
 
   def show_list
